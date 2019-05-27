b71653293ce737acd9d97cc23d835dddf9e87524
Sera <005185709@coyote.csusb.edu>
2019-05-27 16:04:43 -0500

    Added mapper class to data and removed PizzaLogic class from domain.

---
+++ PizzaBoxCustomer/PizzaBoxData/DataDomainMapper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PizzaBoxDomain;
+
+namespace PizzaBoxData
+{
+    public static class DataDomainMapper
+    {
+        public static PizzaTopping DomPizzaTopping2PizzaTopping(DomPizzaTopping inTopping)
+        {
+            PizzaTopping outTopping = new PizzaTopping()
+            {
+                ToppingId = inTopping.ToppingId
+            };
+            return outTopping;
+        }
+
+        public static Pizza DomPizza2Pizza (DomPizza inPizza)
+        {
+            Pizza outPizza = new Pizza()
+            {
+                Crust = inPizza.Crust,
+                Size = inPizza.Size,
+                Cost = inPizza.Cost
+            };
+
+            foreach(var pt in inPizza.PizzaToppings)
+            {
+                outPizza.PizzaTopping.Add(DomPizzaTopping2PizzaTopping(pt));
+            }
+
+            return outPizza;
+        }
+
+        public static Order DomOrder2Order(DomOrder inOrder)
+        {
+            Order outOrder = new Order()
+            {
+                Username = inOrder.Username,
+                LocationId = inOrder.LocationId,
+                OrderDate = inOrder.OrderDate,
+                OrderStatus = inOrder.OrderStatus,
+                Cost = inOrder.Cost
+            };
+            
+            foreach(var p in inOrder.Pizzas)
+            {
+                outOrder.Pizza.Add(DomPizza2Pizza(p));
+            }
+
+            return outOrder;
+        }
+    }
+}
